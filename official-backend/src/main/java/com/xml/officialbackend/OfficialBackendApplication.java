43ce66bc3d15695df825666e235c46963b8774f1
Andrija Vojnovic <andrijavojnovicpa@gmail.com>
2022-01-05 00:46:12 +0100

    initialized official project

---
+++ official-backend/src/main/java/com/xml/officialbackend/OfficialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xml.officialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialBackendApplication.class, args);
+	}
+
+}
---
+++ official-backend/src/test/java/com/xml/officialbackend/OfficialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.officialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
