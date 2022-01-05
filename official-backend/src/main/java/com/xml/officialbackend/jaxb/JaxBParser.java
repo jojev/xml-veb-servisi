17b03f4ec94211f2a4c8d3f7a02202783e8ac0d4
jojev <jo.jevtic999@gmail.com>
2022-01-06 00:20:58 +0100

    added jaxb marshaller

--- official-backend/src/main/java/com/xml/officialbackend/jaxb/JaxBParser.java
+++ official-backend/src/main/java/com/xml/officialbackend/jaxb/JaxBParser.java
@@ -3,18 +3,29 @@ package main.java.com.xml.officialbackend.jaxb;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import org.springframework.stereotype.Component;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XMLResource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
 @Component
 public class JaxBParser {
     public <T> T unmarshall(XMLResource resource, Class genericClass) throws JAXBException, XMLDBException {
         JAXBContext context = JAXBContext.newInstance(genericClass);
         Unmarshaller unmarshaller = context.createUnmarshaller();
-        T createdObject = (T) unmarshaller.unmarshal(resource.getContentAsDOM());
-        return createdObject;
+        return (T) unmarshaller.unmarshal(resource.getContentAsDOM());
+    }
+
+    public <T> OutputStream marshall(Class genericClass,T objectToMarshall) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(genericClass);
+        Marshaller marshaller = context.createMarshaller();
+        OutputStream os = new ByteArrayOutputStream();
+        marshaller.marshal(objectToMarshall, os);
+        return os;
     }
 }
