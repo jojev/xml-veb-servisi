45e89684fa6247122541260510630d77d8acd63e
Andrija Vojnovic <andrijavojnovicpa@gmail.com>
2022-01-06 13:11:58 +0100

    added few changes

--- official-backend/src/main/java/com/xml/officialbackend/existdb/ExistDbManager.java
+++ official-backend/src/main/java/com/xml/officialbackend/existdb/ExistDbManager.java
@@ -6,6 +6,7 @@ import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
 import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.CollectionManagementService;
 import org.xmldb.api.modules.XMLResource;
 import org.exist.xmldb.EXistResource;
 
@@ -37,7 +38,7 @@ public class ExistDbManager {
     public XMLResource load(String collectionUri, String documentId) throws Exception  {
         openConnection();
         Collection collection = null;
-        XMLResource resource;
+        XMLResource resource =  null;
         try {
             collection = DatabaseManager.getCollection(AuthenticationUtilities.loadProperties().uri + collectionUri,
                     AuthenticationUtilities.loadProperties().user,
@@ -46,12 +47,79 @@ public class ExistDbManager {
             resource = (XMLResource) collection.getResource(documentId);
             return resource;
         } finally {
-            if (collection != null) {
-                collection.close();
-            }
+            closeConnection(collection,resource);
+
+        }
+
+    }
+
+    public void store(String collectionId, String documentId, String xmlString) throws Exception  {
+        openConnection();
+        Collection col = null;
+        XMLResource res = null;
+        try {
+            col = getOrCreateCollection(collectionId, 0);
+            res = (XMLResource) col.createResource(documentId, XMLResource.RESOURCE_TYPE);
+            res.setContent(xmlString);
+            col.storeResource(res);
+        } finally {
+            closeConnection(col, res);
         }
     }
 
+
+    private static Collection getOrCreateCollection(String collectionUri, int pathSegmentOffset) throws XMLDBException, IOException {
+
+        Collection col = DatabaseManager.getCollection(AuthenticationUtilities.loadProperties().uri + collectionUri,
+                AuthenticationUtilities.loadProperties().user,
+                AuthenticationUtilities.loadProperties().password);
+
+        // create the collection if it does not exist
+        if(col == null) {
+
+            if(collectionUri.startsWith("/")) {
+                collectionUri = collectionUri.substring(1);
+            }
+
+            String pathSegments[] = collectionUri.split("/");
+
+            if(pathSegments.length > 0) {
+                StringBuilder path = new StringBuilder();
+
+                for(int i = 0; i <= pathSegmentOffset; i++) {
+                    path.append("/" + pathSegments[i]);
+                }
+
+                Collection startCol = DatabaseManager.getCollection(AuthenticationUtilities.loadProperties().uri + path,
+                        AuthenticationUtilities.loadProperties().user,
+                        AuthenticationUtilities.loadProperties().password);
+
+                if (startCol == null) {
+
+                    // child collection does not exist
+
+                    String parentPath = path.substring(0, path.lastIndexOf("/"));
+                    Collection parentCol = DatabaseManager.getCollection(AuthenticationUtilities.loadProperties().uri + parentPath,
+                            AuthenticationUtilities.loadProperties().user,
+                            AuthenticationUtilities.loadProperties().password);
+
+                    CollectionManagementService mgt = (CollectionManagementService) parentCol.getService("CollectionManagementService", "1.0");
+
+                    System.out.println("[INFO] Creating the collection: " + pathSegments[pathSegmentOffset]);
+                    col = mgt.createCollection(pathSegments[pathSegmentOffset]);
+
+                    col.close();
+                    parentCol.close();
+
+                } else {
+                    startCol.close();
+                }
+            }
+            return getOrCreateCollection(collectionUri, ++pathSegmentOffset);
+        } else {
+            return col;
+        }
+    }
 
 
 
