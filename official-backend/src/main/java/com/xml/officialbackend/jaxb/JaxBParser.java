5e2a99a1f973b988d507ad28641267824e97055e
draganaF <filipovic.dada@gmail.com>
2022-01-07 19:58:48 +0100

    added validation for unmarshalling

--- official-backend/src/main/java/com/xml/officialbackend/jaxb/JaxBParser.java
+++ official-backend/src/main/java/com/xml/officialbackend/jaxb/JaxBParser.java
@@ -1,23 +1,53 @@
 package main.java.com.xml.officialbackend.jaxb;
 
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
+import main.java.com.xml.officialbackend.model.digitalni_sertifikat.DigitalniZeleniSertifikat;
+import main.java.com.xml.officialbackend.model.interesovanje.InteresovanjeZaVakcinisanje;
+import main.java.com.xml.officialbackend.model.izvestaj_o_imunizaciji.IzvestajOImunizaciji;
+import main.java.com.xml.officialbackend.model.obrazac_za_sprovodjenje_imunizacije.ObrazacZaSprovodjenjeImunizacije;
+import main.java.com.xml.officialbackend.model.potvrda_o_vakcinaciji.PotvrdaOVakcinaciji;
+import main.java.com.xml.officialbackend.model.zahtev_za_sertifikat.ZahtevZaIzdavanjeSertifikata;
+import main.java.com.xml.officialbackend.util.ShemaValidationHandler;
 import org.springframework.stereotype.Component;
+import org.xml.sax.SAXException;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XMLResource;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 @Component
 public class JaxBParser {
-    public <T> T unmarshall(XMLResource resource, Class genericClass) throws JAXBException, XMLDBException {
+    private HashMap<Class, String> shemaLocationRegistry = new HashMap<>();
+
+    public JaxBParser() {
+        shemaLocationRegistry.put(DigitalniZeleniSertifikat.class, "./data/digitalni_sertifikat.xsd");
+        shemaLocationRegistry.put(InteresovanjeZaVakcinisanje.class, "./data/interesovanje.xsd");
+        shemaLocationRegistry.put(IzvestajOImunizaciji.class, "./data/izvestaj_o_imunizaciji.xsd");
+        shemaLocationRegistry.put(ObrazacZaSprovodjenjeImunizacije.class, "./data/obrazac_za_sprovodjenje_imunizacije.xsd");
+        shemaLocationRegistry.put(PotvrdaOVakcinaciji.class, "./data/potvrda_o_vakcinaciji.xsd");
+        shemaLocationRegistry.put(ZahtevZaIzdavanjeSertifikata.class, "./data/zahtev_za_sertifikat.xsd");
+    }
+    public <T> T unmarshall(XMLResource resource, Class genericClass) throws JAXBException, XMLDBException, SAXException {
         JAXBContext context = JAXBContext.newInstance(genericClass);
         Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new File(shemaLocationRegistry.get(genericClass)));
+
+        unmarshaller.setSchema(schema);
+        unmarshaller.setEventHandler(new ShemaValidationHandler());
+
         return (T) unmarshaller.unmarshal(resource.getContentAsDOM());
     }
 
