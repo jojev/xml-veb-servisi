75156f10cacedaf63d2f5a24d53ee4fe5932d68c
jojev <jo.jevtic999@gmail.com>
2022-01-10 00:27:15 +0100

    refactored done and bug fixed

--- official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
+++ official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
@@ -3,11 +3,13 @@ package main.java.com.xml.officialbackend.existdb;
 import main.java.com.xml.officialbackend.jaxb.JaxBParser;
 import main.java.com.xml.officialbackend.model.digitalni_sertifikat.DigitalniZeleniSertifikat;
 import main.java.com.xml.officialbackend.model.interesovanje.InteresovanjeZaVakcinisanje;
+import main.java.com.xml.officialbackend.rdf.FusekiWriter;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.xmldb.api.modules.XMLResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
