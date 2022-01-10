045eba9dd7762403a372be095fc09463aba5afa7
jojev <jo.jevtic999@gmail.com>
2022-01-10 18:36:09 +0100

    refactored data folder

--- official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
+++ official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
@@ -23,7 +23,7 @@ public class Test {
     private JaxBParser jaxBParser;
 
     public void test() throws Exception {
-        File file = new File("./data/digitalni_sertifikat.xml");
+        File file = new File("./data/documents/digitalni_sertifikat.xml");
         existDbManager.store("/db/digitalni_sertifikat", "2.xml", FileUtils.readFileToString(file, StandardCharsets.UTF_8));
         XMLResource resource = existDbManager.load("/db/digitalni_sertifikat", "2.xml");
 
