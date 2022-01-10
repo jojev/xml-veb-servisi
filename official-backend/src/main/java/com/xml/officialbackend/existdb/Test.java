a6a50d19c3fa7133907942926bad583d22465226
draganaF <filipovic.dada@gmail.com>
2022-01-10 16:11:36 +0100

    added rdfs and rdfa for interesovanje

--- official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
+++ official-backend/src/main/java/com/xml/officialbackend/existdb/Test.java
@@ -25,21 +25,21 @@ public class Test {
     private JaxBParser jaxBParser;
 
     public void test() throws Exception {
+
+        File file = new File("./data/interesovanje.xml");
+        existDbManager.store("/db/interesovanje", "2.xml", FileUtils.readFileToString(file, StandardCharsets.UTF_8));
+        XMLResource resource = existDbManager.load("/db/interesovanje", "2.xml");
+
+        InteresovanjeZaVakcinisanje interesovanje = jaxBParser.unmarshall(resource, InteresovanjeZaVakcinisanje.class);
+
+        OutputStream os = jaxBParser.marshall(InteresovanjeZaVakcinisanje.class, interesovanje);
+
+        existDbManager.store("/db/interesovanje", "2.xml", os.toString());
+
+
         /*
-        File file = new File("./data/digitalni_sertifikat.xml");
-        existDbManager.store("/db/digitalni_sertifikat", "2.xml", FileUtils.readFileToString(file, StandardCharsets.UTF_8));
-        XMLResource resource = existDbManager.load("/db/digitalni_sertifikat", "2.xml");
-
-        DigitalniZeleniSertifikat interesovanje = jaxBParser.unmarshall(resource, DigitalniZeleniSertifikat.class);
-        interesovanje.getLicniPodaci().setImePrezime("Imeee ii Prezimeeee");
-
-        OutputStream os = jaxBParser.marshall(DigitalniZeleniSertifikat.class, interesovanje);
-
-        existDbManager.store("/db/digitalni_sertifikat", "2.xml", os.toString()); */
-
-
         try(RDFReadResult result = FusekiReader.readRDF("/test")) {
             ResultSetFormatter.outputAsXML(result.getResult());
-        }
+        } */
     }
 }
