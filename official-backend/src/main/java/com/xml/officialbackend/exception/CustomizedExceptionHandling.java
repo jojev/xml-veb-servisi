40f311bb12a5fa0959a7bf3aa536f91425aaac35
jojev <jo.jevtic999@gmail.com>
2022-02-11 23:40:03 +0100

    added username validation

--- official-backend/src/main/java/com/xml/officialbackend/exception/CustomizedExceptionHandling.java
+++ official-backend/src/main/java/com/xml/officialbackend/exception/CustomizedExceptionHandling.java
@@ -17,4 +17,11 @@ public class CustomizedExceptionHandling extends ResponseEntityExceptionHandler 
         response.setMessage("Xml koji ste poslali se ne poklapa sa šemom.");
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ExceptionResponse> handleExceptions(ConflictException exception, WebRequest webRequest) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setMessage(exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
 }
--- official-backend/src/main/java/com/xml/officialbackend/service/contract/IUserService.java
+++ official-backend/src/main/java/com/xml/officialbackend/service/contract/IUserService.java
@@ -1,6 +1,11 @@
 package main.java.com.xml.officialbackend.service.contract;
 
 import main.java.com.xml.officialbackend.model.korisnik.Korisnik;
+import main.java.com.xml.officialbackend.rdf.RDFReadResult;
+import org.apache.jena.rdf.model.RDFNode;
+
+import java.io.IOException;
 
 public interface IUserService extends IService<Korisnik> {
+    RDFNode getUserWithUsername(String username) throws IOException;
 }
