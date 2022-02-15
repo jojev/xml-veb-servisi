ad2b9f5381997c6a26871f50b43bb9017b6682cf
jojev <jo.jevtic999@gmail.com>
2022-02-15 02:25:31 +0100

    added finding all vaccine status

--- official-backend/src/main/java/com/xml/officialbackend/controller/VaccineStatusController.java
+++ official-backend/src/main/java/com/xml/officialbackend/controller/VaccineStatusController.java
@@ -1,11 +1,14 @@
 package main.java.com.xml.officialbackend.controller;
 
+import main.java.com.xml.officialbackend.model.dto.StanjeVakcineList;
 import main.java.com.xml.officialbackend.model.stanjevakcine.StanjeVakcine;
 import main.java.com.xml.officialbackend.service.implementation.VaccineStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/v1/vaccine-status", produces={"application/xml"})
@@ -20,6 +23,13 @@ public class VaccineStatusController {
 
     @PutMapping("/{id}")
     private ResponseEntity<?> updateVaccineStatus(@RequestBody StanjeVakcine vaccineStatus, @PathVariable String id) throws Exception {
-        return new ResponseEntity<>(vaccineStatusService.update(vaccineStatus, id), HttpStatus.CREATED);
+        return new ResponseEntity<>(vaccineStatusService.update(vaccineStatus, id), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    private ResponseEntity<StanjeVakcineList> getAllVaccineStatus() throws Exception {
+        List<StanjeVakcine> stanjeVakcineList = vaccineStatusService.findAll();
+        StanjeVakcineList list = new StanjeVakcineList(stanjeVakcineList);
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
---
+++ official-backend/src/main/java/com/xml/officialbackend/model/dto/StanjeVakcineList.java
@@ -0,0 +1,28 @@
+package main.java.com.xml.officialbackend.model.dto;
+
+import main.java.com.xml.officialbackend.model.stanjevakcine.StanjeVakcine;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement
+@XmlSeeAlso({StanjeVakcine.class})
+public class StanjeVakcineList {
+    private List<StanjeVakcine> listOfEntityObjects;
+
+    public StanjeVakcineList() {
+        listOfEntityObjects = new ArrayList<StanjeVakcine>();
+    }
+
+    public StanjeVakcineList(List<StanjeVakcine> listOfEntityObjects) {
+        this.listOfEntityObjects = listOfEntityObjects;
+    }
+
+    @XmlAnyElement
+    public List<StanjeVakcine> getItems() {
+        return listOfEntityObjects;
+    }
+}
