4154f2b65af06174c9af9060eac039ce0f684eb6
Andrija Vojnovic <andrijavojnovicpa@gmail.com>
2022-02-18 14:42:13 +0100

    added navbar without authorization

--- official-frontend/official-frontend/src/modules/app/app.module.ts
+++ official-frontend/official-frontend/src/modules/app/app.module.ts
@@ -7,17 +7,25 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorInterceptor } from '../shared/interceptors/interceptor.interceptor';
 import { ToastrModule } from 'ngx-toastr';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RootLayoutComponent } from './root-layout/root-layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootLayoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
     ToastrModule.forRoot({
       positionClass :'toast-top-center'
     })
